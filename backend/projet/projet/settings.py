95b92451557527a9cb45e09c539653e36723da28
christ <christikonga83@gmail.com>
2022-04-11 17:24:56 +0200

    all

--- backend/projet/projet/settings.py
+++ backend/projet/projet/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'compte.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS =  True
+EMAIL_PORT =  587
+EMAIL_HOST_USER = 'farfanet22@gmail.com'
+EMAIL_HOST_PASSWORD = 'Farafnet007'
+EMAIL_USE_TLS: True
